e478b0e8b1172494c269ba8ec4317cdc66ae28f4
MarlonChi <marlonchiodelli@hotmail.com>
2024-04-01 20:50:56 -0300

    feat: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,18 @@
+import nwlUniteIcon from "../assets/logo.svg";
+
 export const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className="flex items-center gap-5 py-2">
+      <img src={nwlUniteIcon} />
+
+      <nav className="flex items-center gap-5">
+        <a href="" className="font-medium text-sm text-zinc-300">
+          Eventos
+        </a>
+        <a href="" className="font-medium text-sm">
+          Participantes
+        </a>
+      </nav>
+    </div>
+  );
 };
